9d96e9fde778ae2f9373cad5e9422522ae876868
sureshkardhala <sureshkardhala2k22@gmail.com>
2023-07-12 13:07:33 +0530

    Basic printing patterns

---
+++ Patterns/ReverseRightAngle.java
@@ -0,0 +1,17 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class ReverseRightAngle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int n = sc.nextInt();
+        for(int i=0; i<n; i++){
+            for(int j=i; j<n; j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
