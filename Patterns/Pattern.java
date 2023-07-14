c416cab6d52859c0f32cb5221e36db3206ed0efc
sureshkardhala <sureshkardhala2k22@gmail.com>
2023-07-14 18:15:32 +0530

    New patterns are added

--- Patterns/Pattern.java
+++ Patterns/Pattern.java
@@ -95,12 +95,83 @@ public class Pattern {
         pattern3(n-1);
     }
 
-    
+    public static void pattern11(int n ){
+        for(int i=0; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                if((i+j) % 2 == 0){
+                    System.out.print(1 + " ");
+                } else{
+                    System.out.print(0 + " ");
+                }
+            }
+            System.out.println(" ");
+        }
+    }
+
+    public static void pattern12(int n){
+        int space = 2*(n-1);
+        for(int i=1; i<=n ; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(j+" ");
+            }
+            for(int j=1;j<=space; j++ ){
+                System.out.print(" ");
+            }
+
+            for(int j=i; j>=1; j--){
+                System.out.print(j+" ");
+            }
+            space-=2;
+             System.out.println(" ");
+        }
+
+        
+       
+    }
+
+    private static void pattern13(int n) {
+         int space = 1;
+        for(int i=1; i<=n ; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(space+" ");
+                space++;
+            }
+             System.out.println(" ");
+        }
+    }
+
+    private static void pattern14(int n) {
+        for(int i=1; i<=n; i++){
+            int charVal =65;
+            for(int j=1; j<=i; j++){
+                System.out.print((char)charVal+" ");
+                charVal++;
+            }
+            System.out.println(" ");
+        }
+    }
+
+     private static void pattern15(int n) {
+        for(int i=n; i>=1; i--){
+            int charVal =65;
+            for(int j=1; j<=i; j++){
+                System.out.print((char)charVal+" ");
+                charVal++;
+            }
+            System.out.println(" ");
+        }
+    }
+
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number : ");
-        int n = sc.nextInt();
-        pattern10(n);
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the number : ");
+            int n = sc.nextInt();
+            pattern16(n);
+        }
     }
+    
+   
+    
+    
 }
