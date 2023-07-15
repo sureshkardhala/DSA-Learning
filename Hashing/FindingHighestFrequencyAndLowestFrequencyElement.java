d89b83032543a5e4f63ad9954cdeeff845444229
sureshkardhala <sureshkardhala2k22@gmail.com>
2023-07-15 10:48:55 +0530

    Finding highest and lowest frequency element using hashing

--- Hashing/FindingHighestFrequencyAndLowestFrequencyElement.java
+++ Hashing/FindingHighestFrequencyAndLowestFrequencyElement.java
@@ -1,5 +1,36 @@
 package Hashing;
 
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Scanner;
+
+import hMap.EntrySet;
+
 public class FindingHighestFrequencyAndLowestFrequencyElement {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array : ");
+        int n= sc.nextInt();
+        Integer [] arr = new Integer[n];
+        HashMap<Integer, Integer> hMap = new HashMap<>();
+        System.out.println("Enter the elements of an array : ");
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+            hMap.put(arr[i], hMap.getOrDefault(arr[i], 0)+1);
+        }
+
+        int lowFreq=hMap.get(arr[0]),highFreq=hMap.get(arr[0]),hElement=arr[0],lowElement=arr[0];
+        for(Entry<Integer, Integer> e: hMap.entrySet()){
+            if(lowFreq > e.getKey()){
+                lowFreq=e.getValue();
+                lowElement=e.getKey();
+            }
+            if(highFreq<e.getKey()){
+                highFreq=e.getKey();
+                hElement=e.getValue();
+            }
+        }
+        System.out.println(lowElement + " : " +  lowFreq);
+        System.out.println(hElement  + " : " + highFreq);
+    }
 }
