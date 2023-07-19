97641424a57d1ce68fb15cf6cb7a33ecdd8dc350
sureshkardhala <sureshkardhala2k22@gmail.com>
2023-07-19 17:01:58 +0530

    Stock sell and Buy

---
+++ Arrays/Medium/StockSellBuy.java
@@ -0,0 +1,13 @@
+package Arrays.Medium;
+
+public class StockSellBuy {
+    public int maxProfit(int[] prices) {
+        int maxProfit =0;
+        int minPrice = Integer.MAX_VALUE;
+        for(int i=0; i<prices.length; i++){
+            minPrice = Math.min(minPrice, prices[i]);
+            maxProfit=Math.max(maxProfit, prices[i]-minPrice);
+        }
+        return maxProfit;
+    }
+}
