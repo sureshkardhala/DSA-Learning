d7950856aaf9ed07a41acea11955d761f6355b9c
sureshkardhala <sureshkardhala2k22@gmail.com>
2023-07-20 12:15:41 +0530

    SetMatrix zeros

---
+++ Arrays/Medium/SetMatrixZeros.java
@@ -0,0 +1,26 @@
+package Arrays.Medium;
+
+public class SetMatrixZeros {
+    public void setZeroes(int[][] matrix) {
+        //Better appraoch
+        int n = matrix.length;
+        int m= matrix[0].length;
+        int [] row = new int[n];
+        int [] col = new int[m];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j] == 0){
+                    col[j]=1;
+                    row[i]=1;
+                }
+            }
+        }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(col[j]==1 || row[i]==1){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+    }
+}
