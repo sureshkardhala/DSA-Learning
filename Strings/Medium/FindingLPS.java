cc68fd679c688a63bd38151c8970ea1f52fd7d80
sureshkardhala <sureshkardhala2k22@gmail.com>
2023-08-08 18:24:18 +0530

    Finding LongestCommonPrefixAndSuffixSum Optimal solution added

--- Strings/Medium/FindingLPS.java
+++ Strings/Medium/FindingLPS.java
@@ -16,15 +16,37 @@ public class FindingLPS {
         return 0;
     }
     public static void main(String[] args) {
-        String str="ababc";
+        // String str="ababc";
+        // int n=str.length();
+        // int[]arr=new int[n];
+        // for(int i=0; i<n; i++){
+        //     arr[i]=noOfCommonPrefixAndSuffix(str, i+1);
+        // }
+        // for(int i=0; i<n; i++){
+        //     System.out.print(arr[i]+" ");
+        // }
+
+        //Optimal Solution
+        String str="abcabc";
         int n=str.length();
-        int[]arr=new int[n];
-        for(int i=0; i<n; i++){
-            arr[i]=noOfCommonPrefixAndSuffix(str, i+1);
+        int[]lps=new int[n];
+        lps[0]=0;
+        int len=0;
+        int i=1;
+        while(i<n){
+            if(str.charAt(i)==str.charAt(len)){
+                len++;
+                lps[i]=len;
+                i++;
+            }else{
+                if(len==0) {lps[i]=0; i++;}
+                else {len=lps[len-1];}
+            }
         }
-        for(int i=0; i<n; i++){
-            System.out.print(arr[i]+" ");
+        for(int j=0; j<n; j++){
+            System.out.print(lps[j]+" ");
         }
+
        
     }
 }
